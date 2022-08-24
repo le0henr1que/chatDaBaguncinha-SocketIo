392a670156775ef4a947c075d3fc3a84faf960d0
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-08-24 18:11:12 -0300

    git test

--- server.js
+++ server.js
@@ -54,4 +54,6 @@ io.on('connection', socket => {
     });
   });
 
-server.listen(PORT)
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
