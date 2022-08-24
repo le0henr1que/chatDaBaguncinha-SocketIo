846c98079002719824de3410b520902701a534e6
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-08-24 18:15:21 -0300

    teste heroku

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
@@ -21,7 +20,7 @@ app.set('view engine', 'html')
 
 
 app.get('/', (req, res) => {
-    res.renderFile(__dirname+'/public/index.html')
+    res.render('index.html')
 })
 
 let messagem = [];
