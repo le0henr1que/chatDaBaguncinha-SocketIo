9822e19fc44d84ba522b9cf852a97d23686b5e75
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-08-24 17:49:07 -0300

    alter location

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.set('views', path.join(__dirname, '../public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
