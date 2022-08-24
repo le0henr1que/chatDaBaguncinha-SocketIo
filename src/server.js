13396babbfc3b96da87576113eaf6add400678c3
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-08-24 17:16:33 -0300

    deplay test

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const PORT = process.env.PORT || 4000
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../public'));
@@ -21,21 +22,20 @@ let messagem = [];
 
 const users = {};
 
-let userOnline = []
 
 io.on('connection', socket => {
+
     console.log(`Socket conectado: ${socket.id}`);
     socket.emit('previusMessages', messagem)
-   socket.on('sendMessage', data => {
-      messagem.push(data)
-      socket.broadcast.emit('receivedMessage', data)
-    });
+    
+    socket.on('sendMessage', data => {
+        messagem.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+      });
+
     socket.on('login', function(data){
       console.log('a user ' + data.userId + ' connected');
 
-      // console.log(`users connected ${userOnline++ }`)
-      userOnline.push(data.userId)
-      console.log(JSON.stringify(userOnline))
       // saving userId to object with socket ID
       users[socket.id] = data.userId;
     });
@@ -43,10 +43,9 @@ io.on('connection', socket => {
     socket.on('disconnect', function(){
       console.log('user ' + users[socket.id] + ' disconnected');
       // remove saved socket from users object
-      console.log(`users connected ${userOnline - 1}`)
 
       delete users[socket.id];
     });
   });
 
-server.listen(4000)
+server.listen(PORT)
