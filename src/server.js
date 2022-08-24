67ceb72cb91dc4317d264766cfb099b04ed30b3e
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2022-08-24 17:27:24 -0300

    config deploy

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
+const { fileURLToPath } = require('url')
 
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 4000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, '../public'));
@@ -14,7 +18,7 @@ app.set('view engine', 'html')
 
 
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render('index.html')
 })
 
